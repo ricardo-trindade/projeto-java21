fa4e562581a6ef5ab9c21bddf3cbd037e59b6ec0
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-02-23 10:47:37 -0300

    first commit

---
+++ src/main/java/com/ricardo/projetojava21/Projetojava21Application.java
@@ -0,0 +1,13 @@
+package com.ricardo.projetojava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetojava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetojava21Application.class, args);
+	}
+
+}
---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public ResponseEntity<?> github() {
+        return ResponseEntity.ok("CI/CD via Github");
+    }
+
+}
